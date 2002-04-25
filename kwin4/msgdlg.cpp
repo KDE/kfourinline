b1a98e7f8ddd8909df599ec7dac3ccdbd13a1a4c
Stephan Binner <binner@kde.org>
2002-04-25 11:07:24 +0000

    CVS_SILENT Style guide fixes.
    svn path=/trunk/kdegames/kwin4/; revision=151849

--- kwin4/msgdlg.cpp
+++ kwin4/msgdlg.cpp
@@ -47,7 +47,7 @@ MsgDlg::MsgDlg( QWidget *parent, const char *name,const char * /*sufi*/ )
   resize( 400, 160 );
 
   QGroupBox* grp;
-  grp = new QGroupBox(i18n("Enter Eessage"), this);
+  grp = new QGroupBox(i18n("Enter Message"), this);
   grp->resize(380,100);
   grp->move(10,10);
 
