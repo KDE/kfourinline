d15539fddacafe2eacc9ed57f8fde2f9326bb2d2
Stephan Kulow <coolo@kde.org>
2005-10-03 17:46:54 +0000

    first round of porting
    svn path=/trunk/KDE/kdegames/kwin4/; revision=466923

--- kwin4/main.cpp
+++ kwin4/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
   }
   
   KApplication app(argc, argv);
-  KGlobal::locale()->insertCatalogue("libkdegames");
+  KGlobal::locale()->insertCatalog("libkdegames");
 
   if (app.isRestored())
   {
