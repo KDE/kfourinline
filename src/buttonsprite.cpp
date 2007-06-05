8d99d34c5719e079fd26fbce96e23feaf33d7981
Martin Heni <martin@heni-online.de>
2007-06-05 16:42:57 +0000

    Missing kwin4 files
    svn path=/trunk/KDE/kdegames/kwin4/; revision=671859

---
+++ src/buttonsprite.cpp
@@ -0,0 +1,213 @@
+/*
+   This file is part of the KDE games kwin4 program
+   Copyright (c) 2006 Martin Heni <kde@heni-online.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+// Header includes
+#include "buttonsprite.h"
+
+// General includes
+#include <math.h>
+
+// Qt includes
+#include <QSizeF>
+#include <QFont>
+#include<QGraphicsSceneMouseEvent>
+
+// KDE includes
+#include <kdebug.h>
+#include <kconfig.h>
+#include <klocale.h>
+
+// Constructor for the score sprite
+ButtonSprite::ButtonSprite(bool pushButton, const QString &id, ThemeManager* theme, int advancePeriod, int no, QGraphicsScene* scene)
+           :  Themable(id, theme), PixmapSprite(advancePeriod, no, scene)
+{
+
+  mButtonPressed = false;
+  mHover         = false;
+  mPushButton    = pushButton;
+
+  mSignal = new SpriteNotify(this);
+
+  setAcceptedMouseButtons(Qt::NoButton);
+  setAcceptsHoverEvents(false);
+
+  // Create sub sprites
+  mText  = new QGraphicsTextItem(this, scene);
+  mText->hide();
+
+  // Redraw us
+  if (theme) theme->updateTheme(this);
+}
+
+
+
+// Destructor
+ButtonSprite::~ButtonSprite()
+{
+  // Clean up
+  delete mText;
+}
+
+
+// Redraw the theme.
+void ButtonSprite::changeTheme()
+{
+  // The main display is handled by the parent
+  PixmapSprite::changeTheme();
+  
+  // Retrieve our size
+  double width  = this->boundingRect().width();
+  double height = this->boundingRect().height();
+
+  // Retrieve theme data
+  KConfigGroup config = thememanager()->config(id());
+
+  // Text available?
+  if (mText->isVisible())
+  {
+    // Calculate proper font size
+    double fontHeight = config.readEntry("fontHeight", 1.0);
+    fontHeight *= height;
+    double fontWidth = config.readEntry("fontWidth", 1.0);
+    fontWidth *= width;
+    
+    // Retrieve font color
+    QColor fontColor;
+    fontColor = config.readEntry("fontColor", QColor(Qt::white));
+
+    // Create and set current font
+    QFont font;
+    font.setPixelSize(int(fontHeight));
+
+    // Set font and color for all text items
+    mText->setFont(font);   
+    mText->setDefaultTextColor(fontColor);   
+    mText->setTextWidth(fontWidth);   
+
+    // Set position of sub sprites
+    double textWidth  = mText->boundingRect().width();
+    double textHeight = mText->boundingRect().height();
+    mText->setPos((width-textWidth)/2.0, (height-textHeight)/2.0);
+  }
+}
+
+
+// QGI advance method 
+void ButtonSprite::advance(int phase)
+{
+  // Advance time and animation etc
+  PixmapSprite::advance(phase);
+}
+
+
+// Set the button status (on or off)
+void ButtonSprite::setStatus(const bool status)
+{
+  mButtonPressed = status;
+  changeFrame();
+}
+
+
+// Get the button status (on or off)
+bool ButtonSprite::status() const
+{
+  return mButtonPressed;
+}
+
+
+// Store and display the button text
+void ButtonSprite::setText(const QString &s)
+{
+  if (s.isNull()) 
+  {
+    mText->hide();
+  }
+  else
+  {
+    //mText->setPlainText(s);
+    // Center
+    mText->setHtml("<div align=\"center\">"+s+"</div>");
+    mText->show();
+    thememanager()->updateTheme(this);
+  }
+}
+
+
+// Adjust the frame depending on the state
+void ButtonSprite::changeFrame()
+{
+  int frame = 0;
+  if (mButtonPressed) frame = 2;
+  if (mHover) frame += 1;
+  setFrame(frame);
+}
+
+
+// Hover enter event recevied
+void ButtonSprite::hoverEnterEvent(QMouseEvent* /*event*/)  
+{
+  mHover = true;
+  changeFrame();
+}
+
+
+// Hover leave event received
+void ButtonSprite::hoverLeaveEvent(QMouseEvent* /*event*/)
+{
+  mHover = false;
+  changeFrame();
+}
+
+
+// Mouse press event received
+void ButtonSprite::mousePressEvent(QMouseEvent* /*event*/)
+{
+  if (mPushButton) 
+  {
+    mButtonPressed = !mButtonPressed;
+    if (mButtonPressed) mSignal->emitSignal(number()|0x100);
+    else mSignal->emitSignal(number());
+  }
+  changeFrame();
+}
+
+
+// Mouse release event received
+void ButtonSprite::mouseReleaseEvent(QMouseEvent* event)
+{
+  QPointF p = mapFromScene (QPointF(event->pos()) );
+  // kDebug() << "ButtonSprite::mouseReleaseEvent contains?"<< contains(p) << endl;
+  if (!contains(p)) 
+  {
+    mHover = false;
+    if (mPushButton) mButtonPressed = !mButtonPressed;
+  }
+  else
+  {
+    if (!mPushButton)
+    {
+      mButtonPressed = !mButtonPressed;
+      if (mButtonPressed) mSignal->emitSignal(number()|0x100);
+      else mSignal->emitSignal(number());
+
+    }
+  }
+  changeFrame();
+}
---
+++ src/buttonsprite.h
@@ -0,0 +1,140 @@
+#ifndef BUTTON_SPRITE_H
+#define BUTTON_SPRITE_H
+/*
+   This file is part of the KDE games kwin4 program
+   Copyright (c) 2006 Martin Heni <kde@heni-online.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+// Qt includes
+#include <QPointF>
+#include <QSizeF>
+#include <QEvent>
+#include <QMouseEvent>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsTextItem>
+
+
+// Local includes
+#include "thememanager.h"
+#include "pixmapsprite.h"
+#include "spritenotify.h"
+
+
+/** The sprite for a interactive buttons. The button can either be
+  * a push button which reacts on mouse press events or a toggle button
+  * which is toggled on mouse release.
+  *
+  * NOTE: An own event handler is implemeted currently in the DisplayIntro
+  *       class which uses this buttons because the Qt4.3 QGraphicsView event
+  *       handling system is faulty!!!!
+ */
+class ButtonSprite : public PixmapSprite
+{
+  public:
+    /** Constructor for the score sprite.
+      * @param pushButton      True: push button, False: Toggle button
+      * @param id              The theme id
+      * @param theme           The theme manager
+      * @param advancePeriod   The canvas advance period [ms]
+      * @param no              A used defined number (unused)
+      * @param scene           The graphics scene
+      */
+    ButtonSprite(bool pushButton, const QString &id, ThemeManager* theme, int advancePeriod, int no, QGraphicsScene* scene);
+    
+    /** Destructor 
+      */
+    ~ButtonSprite();
+
+    /** Standard QGI advance function.
+     *  @param phase The advance phase
+     */
+    virtual void advance(int phase);
+
+    /** Retrieve the type of QGI. This item is UserType+10
+     *  @return The type of item.
+     */
+    virtual int type() const {return QGraphicsItem::UserType+100;}
+
+    /** Main theme change function. On call of this the item needs to redraw and
+      * resize.
+      */
+    virtual void changeTheme();
+
+    /** Store the button text.
+      * @param s  The text
+      */ 
+    void setText(const QString &s);
+
+    /** Set the status of the button to on or off.
+      * @param status The status
+      */
+    void setStatus(const bool status);  
+
+    /** Retrieve the status of the button (on or off).
+      * @return The status
+      */
+    bool status() const;
+
+    /** Allow the sprite to emit a signal. Used for button events.
+      * @return The notify object.
+      */
+    SpriteNotify* notify() {return mSignal;}
+
+    /** A mouse press event is received.
+      * @param event The mouse event
+      */
+    virtual void mousePressEvent(QMouseEvent* event);
+
+    /** A mouse release event is received.
+      * @param event The mouse event
+      */
+    virtual void mouseReleaseEvent(QMouseEvent* event);
+
+    /** A hover in event is reveived (mouse tracking must be on).
+      * @param event The mouse event
+      */
+    virtual void hoverEnterEvent(QMouseEvent* event);  
+
+    /** A hover out event is reveived (mouse tracking must be on).
+      * @param event The mouse event
+      */
+    virtual void hoverLeaveEvent(QMouseEvent* event);  
+
+
+  protected:
+    /** Change the frame of the sprite to match the button state.
+      */
+    void changeFrame();
+
+    
+  private:
+    // Button text 
+    QGraphicsTextItem* mText;
+    // State of button
+    bool mButtonPressed;
+    // State of hover
+    bool mHover;
+    // Is push button?
+    bool mPushButton;
+
+    // The notification object
+    SpriteNotify* mSignal;
+
+};
+
+#endif
