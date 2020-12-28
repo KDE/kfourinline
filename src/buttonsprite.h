fd57f12caf61b685db842adb55b1b58bfdf3edcd
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-28 07:07:49 +0100

    Convert license statements to SPDX expressions
    Also add/move license files as required by REUSE specification
    
    GIT_SILENT

--- src/buttonsprite.h
+++ src/buttonsprite.h
@@ -1,24 +1,12 @@
+/*
+    This file is part of the KDE games kwin4 program
+    SPDX-FileCopyrightText: 2006 Martin Heni <kde@heni-online.de>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
 #ifndef BUTTON_SPRITE_H
 #define BUTTON_SPRITE_H
-/*
-   This file is part of the KDE games kwin4 program
-   Copyright (c) 2006 Martin Heni <kde@heni-online.de>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
 
 // Qt includes
 #include <QMouseEvent>
